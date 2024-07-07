c7c00352d75d98b4ca41bd1c235ffd527ffe29f8
shigerubt <z648s.7bt@gmail.com>
2024-07-06 21:23:41 -0600

    chore: Remove Counter and CounterScript contracts, add UserRegistry and CuchubalFactory contracts

---
+++ src/UserRegistry.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract UserRegistry {
+    struct User {
+        address wallet;
+        string nickname;
+        bool isRegistered;
+    }
+
+    //testing
+
+    mapping(address => User) public users;
+
+    event UserRegistered(address indexed wallet);
+
+    function registerUser() public {
+        require(!users[msg.sender].isRegistered, "Ya estas registrado");
+
+        users[msg.sender] = User({
+            wallet: msg.sender,
+            nickname: "", // Cual es tu nickname?
+            isRegistered: true
+        });
+
+        emit UserRegistered(msg.sender);
+    }
+
+    function updateNickname(string memory newNickname) public {
+        require(!users[msg.sender].isRegistered, "No estas registrado");
+
+        users[msg.sender].nickname = newNickname;
+    }
+
+    function isUserRegistered(address _wallet) public view returns (bool) {
+        return users[_wallet].isRegistered;
+    }
+}
